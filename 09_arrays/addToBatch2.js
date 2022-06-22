c002831886a432ee9de54fe2c9941d4c3150efc3
Clare Budds <clarebudds@yahoo.co.uk>
2022-06-22 16:03:51 +0100

    Phase1 Exercise9 arrays

---
+++ 09_arrays/addToBatch2.js
@@ -0,0 +1,11 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const addToBatch = (array, number) => {
+  if (array.length >= 5) {
+    return array;
+  }
+
+  return array.concat(number);
+};
+
+console.log(addToBatch([1], 3));
---
+++ 09_arrays/array.js
@@ -0,0 +1,6 @@
+const array = ["Clare", "Rebecca", "Rachel"];
+console.log(array[1]);
+
+const newArray = array.concat("Aisha");
+
+console.log(newArray);
