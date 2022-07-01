78d2b8e0d2c39b7db9daa2a23c451d1d2d373455
Clare Budds <clarebudds@yahoo.co.uk>
2022-07-01 22:18:40 +0100

    Phase1 Exercise9 Arrays

---
+++ 9_arrays/names.js
@@ -0,0 +1,9 @@
+const names = ["Clare", "Rebecca", "Rachel"];
+
+console.log(names[1]);
+// Rebecca
+
+const newNames = names.concat("Aisha");
+
+console.log(newNames);
+// [ 'Clare', 'Rebecca', 'Rachel', 'Aisha' ]
---
+++ 9_arrays/numbers.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let result = 0;
+
+numbers.forEach((number) => {
+  result += number;
+});
+
+console.log(result);
+// 55
